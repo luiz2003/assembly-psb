31d2f10f2100b0fadcb8305a984b89bacf3624da
Luiz Valverde <luizcastroval321@gmail.com>
2022-11-24 12:36:42 -0300

    feat: take string from input

--- main.asm
+++ main.asm
@@ -1,8 +1,19 @@
 %include "io.inc"
 
+section .bss
+
+string resb 66
+
+sub_string resb 41
+
 section .text
 global CMAIN
+
 CMAIN:
     ;write your code here
     xor eax, eax
+    
+    GET_STRING string, 66  
+    
+    
     ret
